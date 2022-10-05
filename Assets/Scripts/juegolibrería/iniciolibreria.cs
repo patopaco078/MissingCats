7e9a88aaddf069b3af68b143b654ab8111702bd7
Manuelxcool1997 <98336326+Manuelxcool1997@users.noreply.github.com>
2022-10-05 16:23:19 -0500

    c

--- Assets/Scripts/juegolibrería/iniciolibreria.cs
+++ Assets/Scripts/juegolibrería/iniciolibreria.cs
@@ -282,7 +282,7 @@ public Button ingresar;
 
                        
                         retornoContraseña.gameObject.SetActive(true);
-                        cambio.SetActive(false);
+                     
                         codigo.gameObject.SetActive(true);
 
                         ingresar.gameObject.SetActive(true);
