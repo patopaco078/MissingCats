fb6f14d594a2fb8cdd844f7daff701d848bb1667
Tomas Alvarez <tomas.alvarez@upb.edu.co>
2022-11-01 16:15:35 -0500

    Organizar Room3 LVL 2

--- Assets/Scripts/DOTMove.cs
+++ Assets/Scripts/DOTMove.cs
@@ -10,6 +10,7 @@ public class DOTMove : MonoBehaviour
     [SerializeField] private Transform[] props2;
     [SerializeField] private Transform[] props3;
 
+    [SerializeField] private bool DoLOCAL;
 
     [SerializeField] private float time;
     [SerializeField] private float x=0;
@@ -22,14 +23,21 @@ public class DOTMove : MonoBehaviour
         {
             //Debug.Log(props0.Length);
             //Debug.Log(i);
-            props0[i].DOMove(new Vector3(x, y, z), time);
+
+            if (DoLOCAL == false)
+                props0[i].DOMove(new Vector3(x, y, z), time);
+            if (DoLOCAL == true)
+                props0[i].DOLocalMove(new Vector3(x, y, z), time);
         }
     }
     public void Prop0Down()
     {
         for (int i = 0; i < props0.Length -1; i++)
         {
-            props0[i].DOMove(new Vector3(x, 0, z), time);
+            if (DoLOCAL == false)
+                props0[i].DOMove(new Vector3(x, 0, z), time);
+            if (DoLOCAL == true)
+                props0[i].DOLocalMove(new Vector3(x, 0, z), time);
         }
     }
 
@@ -37,28 +45,40 @@ public class DOTMove : MonoBehaviour
     {
         for (int i = 0; i < props1.Length -1; i++)
         {
-            props1[i].DOMove(new Vector3(x, y, z), time);
+            if (DoLOCAL == false)
+                props1[i].DOMove(new Vector3(x, y, z), time);
+            if (DoLOCAL == true)
+                props1[i].DOLocalMove(new Vector3(x, y, z), time);
         }
     }
     public void Prop1Down()
     {
         for (int i = 0; i < props1.Length -1; i++)
         {
-            props1[i].DOMove(new Vector3(x, 0, z), time);
+            if(DoLOCAL == false)
+                props1[i].DOMove(new Vector3(x, 0, z), time);
+            if (DoLOCAL == true)
+                props1[i].DOLocalMove(new Vector3(x, 0, z), time);
         }
     }
     public void Prop2Up()
     {
         for (int i = 0; i < props2.Length - 1; i++)
         {
-            props2[i].DOMove(new Vector3(x, y, z), time);
+            if (DoLOCAL == false)
+                props2[i].DOMove(new Vector3(x, y, z), time);
+            if (DoLOCAL == true)
+                props2[i].DOLocalMove(new Vector3(x, y, z), time);
         }
     }
     public void Prop2Down()
     {
         for (int i = 0; i < props2.Length - 1; i++)
         {
-            props2[i].DOMove(new Vector3(x, 0, z), time);
+            if (DoLOCAL == false)
+                props2[i].DOMove(new Vector3(x, 0, z), time);
+            if (DoLOCAL == true)
+                props2[i].DOLocalMove(new Vector3(x, 0, z), time);
         }
     }
 
@@ -66,14 +86,20 @@ public class DOTMove : MonoBehaviour
     {
         for (int i = 0; i < props3.Length - 1; i++)
         {
-            props3[i].DOMove(new Vector3(x, y, z), time);
+            if (DoLOCAL == false)
+                props3[i].DOMove(new Vector3(x, y, z), time);
+            if (DoLOCAL == true)
+                props3[i].DOLocalMove(new Vector3(x, y, z), time);
         }
     }
     public void Prop3Down()
     {
         for (int i = 0; i < props3.Length - 1; i++)
         {
-            props3[i].DOMove(new Vector3(x, 0, z), time);
+            if (DoLOCAL == false)
+                props3[i].DOMove(new Vector3(x, 0, z), time);
+            if (DoLOCAL == true)
+                props3[i].DOLocalMove(new Vector3(x, 0, z), time);
         }
     }
 }
